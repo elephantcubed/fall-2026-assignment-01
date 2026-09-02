996949ca2ec6a1b62d35abb2718a10c2895342ee
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 16:57:24 -0400

    exercises 11 & 12 because 8 9 10 are annoying :P

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,9 @@
+import * as fs from "fs";
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const message = `${statusMessage}, ${new Date().toISOString()}\n`;
+  await fs.promises.appendFile(filePath, message);
+}
