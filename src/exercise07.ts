d4a05b5c768af7737b5125c2719a32859d7d88d6
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 16:29:18 -0400

    exercise 7

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,23 @@
+import * as fs from "fs";
+
+export type Gradebook = {
+  [studentName: string]: {
+    [subject: string]: number;
+  }
+}
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+  const rawData = fs.readFileSync("data/gradebook.json", "utf-8");
+  const gb: Gradebook = JSON.parse(rawData);
+
+  let count = 0;
+  let total = 0;
+
+  for (const student in gb) {
+    if (gb[student][subject]) {
+      total += gb[student][subject];
+      count++
+    }
+  }
+  return count > 0 ? total / count : 0;
 }
