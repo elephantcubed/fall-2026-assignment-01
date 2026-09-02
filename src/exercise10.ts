9e784e7e4b48edc7b2e4ab507d20f1e456804a20
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 18:45:12 -0400

    exercise 10 was such a pain, 15 will not be getting done in time lmao

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,40 @@
-type UserAccount = {};
+export type UserAccount = {
+  id: string;
+  createdAt: Date;
+  email: string;
+  passwordHash: string;
+  profile: {
+    bio: string;
+    avatarUrl: string;
+  }
+};
 
 export class UserRegistry {
+  public users: UserAccount[] = [];
   public registerUser(
     data: Omit<UserAccount, 'id' | 'createdAt'>,
   ): UserAccount {
-    return {};
+    const user: UserAccount = {
+      id: String(this.users.length),
+      createdAt: new Date(),
+      ...data
+    }
+    this.users.push(user);
+    return user;
   }
 
   public getUserView(
     id: string,
   ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
+    for (const user of this.users) {
+      if (user.id == id) {
+        return Object.freeze({
+          id: user.id,
+          email: user.email,
+          profile: user.profile
+        }
+      )}
+    };
     return undefined;
   }
 }
