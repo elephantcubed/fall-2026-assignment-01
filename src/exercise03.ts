acc081778d9ee32fa12b3edb842159359d5c0a76
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 15:48:32 -0400

    exercise 3

--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,7 @@
+import { totalmem } from "os";
+
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  
+return inventory.filter((item) => item[1] > 5).reduce((total, item) => total + item[1] * item[2], 0);
 }
