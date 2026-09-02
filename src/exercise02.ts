8e97de5ef0778b5e40ec52a8be6b6e9831a9bf08
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 15:00:56 -0400

    exercise 2

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,17 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+  const RNA: Record<string, string> = {
+    "A" : "U",
+    "T" : "A",
+    "C" : "G",
+    "G" : "C"
+  };
+
+  let result = "";
+  for (const ch of dna) {
+    if (!(ch in RNA)) {
+      throw new TypeError();
+    }
+    result += RNA[ch];
+  }
+  return result;
 }
--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,5 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
+  
 }
