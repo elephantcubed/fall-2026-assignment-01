2fcaac338256e2a13b0b9d9044afed82eb1919cd
Titus Henry <elephantcubed33@gmail.com>
2026-09-02 17:49:54 -0400

    exercise 13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,22 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+    if (!response.ok) {
+      return null;
+    }
+
+    const item = await response.json() as TodoItem;
+    return item;
+  }
+  catch (error) {
+    return null;
+  }
 }
